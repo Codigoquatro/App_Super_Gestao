b52d056318494c62a3e2aecb3d615e267042b154
Codigoquatro-Antony <codigoquatro2022@gmail.com>
2023-01-10 22:01:21 -0300

    created Project App_Super_Gestao

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return 'ola, estou na aula!';
+});\*/
+
+Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/login', function() { return 'Login';});
+
+Route::get('/clientes', function() { return 'Clientes';});
+
+Route::get('/fornecedores', function() { return 'Fornecedores';});
+
+Route::get('/produtos', function() { return 'Produtos';});
